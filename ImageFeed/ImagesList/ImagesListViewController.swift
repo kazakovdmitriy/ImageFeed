f0cc38fc467dddede8123d7c48245204f4a7f088
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-04-12 17:18:51 +0300

    Добавлен экран профиля

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -12,6 +12,7 @@ final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Дмитрий on 12.04.2024.
 //
 
-import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var avatarImageOutlet: NSLayoutConstraint!
+    @IBOutlet private weak var nameLabelOutlet: UILabel!
+    @IBOutlet private weak var loginLabelOutlet: UILabel!
+    @IBOutlet private weak var statusLabelOutlet: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction private func logoutAction(_ sender: Any) {
+        print("Пользователь хочет выйти")
+    }
+}
