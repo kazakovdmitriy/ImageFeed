7f4e050499690729b062e6b77e49725ea16e6123
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-06-11 00:14:07 +0300

    Добавил тесты для WebView

---
+++ ImageFeedTests/WebViewTest.swift
@@ -0,0 +1,129 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Дмитрий on 10.06.2024.
+//
+
+@testable import ImageFeed
+import XCTest
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: ImageFeed.WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var loadRequestCalled: Bool = false
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+    
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+    
+    func setProgressValue(_ newValue: Float) { }
+    
+    func setProgressHidden(_ isHidden: Bool) { }
+}
+
+final class WebViewTest: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        // given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+    
+    func testPresenterCallsLoadRequest() {
+        // given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let viewController = WebViewViewControllerSpy()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        presenter.viewDidLoad()
+        
+        // then
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+    
+    func testProgressVisibleWhenLessThenOne() {
+        // given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progres: Float = 0.6
+        
+        // when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progres)
+        
+        // then
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProgressHiddenWhenOne() {
+        // given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progres: Float = 1.0
+        
+        // when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progres)
+        
+        // then
+        XCTAssertTrue(shouldHideProgress)
+    }
+    
+    func testAuthHelperAuthURL() {
+        // given
+        let configuration = AuthConfiguration.standart
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        // when
+        let url = authHelper.authURL()
+        let urlString = url?.absoluteString
+        
+        // then
+        XCTAssertTrue(((urlString?.contains(configuration.authURLString)) != nil))
+        XCTAssertTrue(((urlString?.contains(configuration.accessKey)) != nil))
+        XCTAssertTrue(((urlString?.contains(configuration.redirectURI)) != nil))
+        XCTAssertTrue(((urlString?.contains("code")) != nil))
+        XCTAssertTrue(((urlString?.contains(configuration.accessScope)) != nil))
+    }
+    
+    func testCodeFromURL() {
+        // given
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")
+        urlComponents?.queryItems = [URLQueryItem(name: "code", value: "test code")]
+        let url = urlComponents!.url!
+        let authHelper = AuthHelper()
+        
+        // when
+        let code = authHelper.code(from: url)
+        
+        // then
+        XCTAssertEqual(code, "test code")
+    }
+}
