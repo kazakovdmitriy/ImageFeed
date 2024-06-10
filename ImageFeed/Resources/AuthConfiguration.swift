79c5191c5bd5a6daea21fba5172ff3a753323dae
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-06-10 13:39:04 +0300

    Выполнен рефакторинг WebViewViewController, созданы отдельные сущности WebViewPresenter, AuthHelper, AuthConfiguration

---
+++ ImageFeed/Resources/AuthConfiguration.swift
@@ -0,0 +1,46 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Дмитрий on 07.05.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "g5KZBv93MU_RxUaulqnJ_p1G8dxCMTKRZAuiRxpuwsI"
+    static let secretKey = "DHcjxkDT1PeTA8whKhI85MdK0B2RVCb8MXfObqHa9Q8"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+    
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, defaultBaseURL: URL, authURLString: String) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+    
+    static var standart: AuthConfiguration {
+        return AuthConfiguration(
+            accessKey: Constants.accessKey,
+            secretKey: Constants.secretKey,
+            redirectURI: Constants.redirectURI,
+            accessScope: Constants.accessScope,
+            defaultBaseURL: Constants.defaultBaseURL,
+            authURLString: Constants.unsplashAuthorizeURLString)
+    }
+}
